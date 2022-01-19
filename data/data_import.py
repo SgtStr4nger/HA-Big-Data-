43dc79132f13be72f075840b0f42989eb746c147
SirCoCoNut <maltebonn@icloud.com>
2022-01-19 23:24:31 +0100

    Added TODO

--- data/data_import.py
+++ data/data_import.py
@@ -3,3 +3,4 @@ import pandas as pd
 def import_fallzahlen_lk ():
     df = pd.read_excel ('data/raw-data/Fallzahlen_Kum_Tab.xlsx', sheet_name=5 , engine='openpyxl')
     print ( df )
+    #TODO: transform for easier use
