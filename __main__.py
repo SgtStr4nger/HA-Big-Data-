a318cd6477853ed00effd75f78db6da1b2e68e9a
SirCoCoNut <maltebonn@icloud.com>
2022-01-19 23:16:55 +0100

    raw data

--- __main__.py
+++ __main__.py
@@ -1,3 +1,5 @@
-from data import data-import.py
+from data import data_import
+from data.data_import import import_fallzahlen_lk
+import xlrd
 
-import_fallzahlen()
+import_fallzahlen_lk()
--- data/data_import.py
+++ data/data_import.py
@@ -1,5 +1,6 @@
 import pandas as pd
+import openpyxl
 
-def import_Fallzahlen ():
-    df = pd.read_excel ('data/raw-data/Fallzahlen_Kum_Tab.xlsx')
+def import_fallzahlen_lk ():
+    df = pd.read_excel ('data/raw-data/Fallzahlen_Kum_Tab.xlsx', sheet_name=5 , engine='openpyxl')
     print ( df )
