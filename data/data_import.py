d8f4fcd975c6ea768417b75f5ea622d276fdc2ea
SirCoCoNut <maltebonn@icloud.com>
2022-01-20 21:33:26 +0100

    changed dataframe transformation

--- data/data_import.py
+++ data/data_import.py
@@ -4,7 +4,7 @@ def import_fallzahlen_lk ():
     # Load Inzidenz Zahlen pro Landkreis as dataframe
     df = pd.read_excel ('data/raw-data/Fallzahlen_Kum_Tab.xlsx', sheet_name=5 , header=4, engine='openpyxl')
     # removing unnecessary rows at end of dataframe, transform for easier user
-    df = df.drop(range(411,427))
+    df = df.drop(range(411,416))
     df = df.sort_values(by=['NR'])
     df = df.reset_index ( drop=True )
     print ( df )
