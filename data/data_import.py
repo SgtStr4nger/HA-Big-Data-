418a6ddb0d753e4c6b5dc6d939fdeb584f200abf
SirCoCoNut <maltebonn@icloud.com>
2022-01-20 17:24:54 +0100

    Improved importing of Fallzahlen per Landkreis added transformation of dataframe

--- data/data_import.py
+++ data/data_import.py
@@ -1,6 +1,14 @@
 import pandas as pd
 
 def import_fallzahlen_lk ():
-    df = pd.read_excel ('data/raw-data/Fallzahlen_Kum_Tab.xlsx', sheet_name=5 , engine='openpyxl')
+    # Load Inzidenz Zahlen pro Landkreis as dataframe
+    df = pd.read_excel ('data/raw-data/Fallzahlen_Kum_Tab.xlsx', sheet_name=5 , header=4, engine='openpyxl')
+    # removing unnecessary rows at end of dataframe, transform for easier user
+    df = df.drop(range(411,427))
+    df = df.sort_values(by=['NR'])
+    df = df.reset_index ( drop=True )
     print ( df )
-    #TODO: transform for easier use
+
+
+
+#TODO: Add import for other data
