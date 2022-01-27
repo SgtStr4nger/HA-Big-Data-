b712a611b71a77048117e5c96dcda1e78b9c260d
SirCoCoNut <maltebonn@icloud.com>
2022-01-27 23:37:28 +0100

    Editet

--- __main__.py
+++
@@ -1,3 +0,0 @@
-from data.data_import import import_fallzahlen_lk
-
-import_fallzahlen_lk()
--- data/acc_vaccinedata.py
+++ data/acc_vaccinedata.py
@@ -1,12 +1,13 @@
 import csv
-from datetime import date
+import datetime
+import pandas as pd
 from collections import namedtuple, defaultdict
 
 #Data Source Impfungen/Infektionen: RKI https://github.com/robert-koch-institut (25.01.22)
 #Data Source Bevölkerungszahlen: DeStatis https://www.destatis.de/DE/Themen/Laender-Regionen/Regionales/Gemeindeverzeichnis/Administrativ/04-kreise.html (26.01.22 / Datenstand: 31.12.2020)
 # LK 17000 : Bundesressort → Daten aus Impfungen durch den Bund, keine Ortsangabe, ergo keine Bevölkerungszahlen auffindbar
 #Impfungen aufteilen in Grundimmunisiert (=Impfstatus 2, da J&J als zweitimpfungen eingetragen wurden) und Geboostert 
-vaccineData = "Aktuell_Deutschland_Landkreise_COVID-19-Impfungen.csv"
+vaccineData = "raw-data\Aktuell_Deutschland_Landkreise_COVID-19-Impfungen.csv"
 bevData = "raw-data\Bev_Kreise.csv"
 
 #[Kommentar]Datum, Ort, Impfung(Grundimmuniesierung, Booster), Anzahl aus Quelle auslesen
@@ -23,7 +24,7 @@ def read_bevData():
         csvreader = csv.reader(csvfile, delimiter=";")
         data = [[row[0], row[1]] for row in csvreader]
         #Data zu Testzwecken auf alle Datensätze begrenzt
-        return data[1:]
+        return data
 
 def bev_to_Dict():
     bevData = read_bevData()
@@ -48,21 +49,55 @@ def put_Into_Tuple():
 
 vTupel = put_Into_Tuple()
 
+# Erstelle Dict mit LK_ID, Date und Liste [0, 0]
+def createDict ():
+    date_generated = pd.date_range(datetime.date(2019, 6, 1),datetime.date(2022, 2, 16))
+    emptyDict = defaultdict(dict)
+    for lk in bevDict:
+        for Date in date_generated:
+            emptyDict[lk][Date.strftime('%Y-%m-%d')] = [0, 0]
+    return emptyDict
 
 # #[Kommentar]Aus Tupeln ein Nested Dict formen: {LK_ID: {Datum: [Grundimmun][Booster]} }
-def acc_Data():
-    akkData = {}
-    for row in vTupel:
+def acc_Data(empty_dict):
+    akkData = empty_dict
+    for row in vTupel :
         date, lk_id, basicimmun, boost = row[0], row[1], row[2], row[3]
-        #[Kommentar] Check, ob korrespondierende Bevölkerungszahlen vorhanden
-        if row[1] in bevDict:
-            if lk_id in akkData and date in akkData[lk_id]:
-                akkData[lk_id][date[0]] = [[akkData[lk_id][date[0]] + (basicimmun/ bevDict[row[1]])],akkData[lk_id][date[1]] + (boost/ bevDict[row[1]])]
-            else:
-                akkData[lk_id][date[1]] = [ (basicimmun/ bevDict[row[1]]) , (boost/ bevDict[row[1]])  ]
+# [Kommentar] Einordnen der Zahlen in Dict, ausgenommen LK 17000 → Bundesresort
+        if lk_id in akkData and date in akkData[lk_id] and lk_id!=17000:
+            akkData[lk_id].update({date: [x + y for x, y in zip(akkData[lk_id][date], [basicimmun, boost])]})
     return akkData
-accc_DATA = acc_Data()
-print(accc_DATA)
-# vDict = acc_Data(vTupel)
-# print(vDict[5314])
+
+def Dict_kummulieren ( Dict):
+    for lk_id in Dict:
+        for Date in Dict[lk_id]:
+
+            #TODO: Optimize Date-1
+            Date_as_datetime = datetime.datetime.strptime(Date, '%Y-%m-%d')
+            Date_before = Date_as_datetime - datetime.timedelta(days=1)
+            Date_before = datetime.datetime.strftime(Date_before, '%Y-%m-%d')
+# Addiere Impfungen am Date zu denen vom Tag zuvor
+            if Dict[lk_id].get(Date_before) != None:
+                Dict[lk_id].update({Date: [x + y for x, y in zip(Dict[lk_id][Date_before], Dict[lk_id][Date]) ] })
+
+# Teile Impfungen pro Tag durch Bevölkerung des Landkreises
+        for Date in Dict[lk_id]:
+            Dict[lk_id].update({Date: [x / bevDict[lk_id] for x in Dict[lk_id][Date]]})
+
+    return Dict
+
+def V_Datensatz_erstellen():
+    return Dict_kummulieren(acc_Data(createDict()))
+
+def Print_V_Dataset(Data):
+    for lk_id in Data:
+        print(lk_id, ":", Data[lk_id]['2022-02-16'])
+
+
+V_Datensatz = V_Datensatz_erstellen()
+#Print_V_Dataset(V_Datensatz)
+#print(V_Datensatz)
+
+#print(V_Datensatz[16077]['2022-02-16'][0])
+
 
