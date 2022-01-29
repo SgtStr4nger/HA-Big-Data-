2ee5ced93bde5d1fab5fd3527a009365a8151dad
SirCoCoNut <maltebonn@icloud.com>
2022-01-29 14:56:52 +0100

    Adjusted visualization.py according to Impfquote changes, added Plotting of V 1/2 and V 3.

--- visualization/visualization.py
+++ visualization/visualization.py
@@ -13,17 +13,20 @@ def plot_data(V_Data, C_Data):
     y = []
 
 # Für jedes Datum jeder lk_id in CData Neuinfektionen des LK auf Impfquote des BL des LK plotten
-    for i in range (1):
+    for i in range (2):
         for lk_id in C_Data:
             for Date in C_Data[lk_id]:
                 if int(lk_id/1000) in V_Data and Date in V_Data[int(lk_id/1000)]:
                     x.append(V_Data[int(lk_id/1000)][Date][i])
                     y.append(C_Data[lk_id][Date])
 
+        plt.plot(x, y, "r+", markersize=0.1)
+        plt.savefig('line_plot_V' + str(i) + '.png', dpi=500)
+        plt.show()
+        x.clear(), y.clear()
 
-    plt.plot(x, y, "r+" , markersize= 0.1)
-    plt.savefig('line_plot_hq.png', dpi=500)
-    plt.show()
+
+
 
 
 
