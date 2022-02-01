e281a41ee9828e874909e8533695017b68b1c0f8
SirCoCoNut <maltebonn@icloud.com>
2022-02-01 21:46:03 +0100

    Changes during/after group call.

--- data_analysis/data_preparation.py
+++ data_analysis/data_preparation.py
@@ -1,18 +1,31 @@
 # Preparing data for analysis
 from collections import defaultdict
 
-import pandas as pd
+import pandas as pd, matplotlib.pyplot as plt
 
 from data.acc_covdata import C_Datensatz_erstellen, C_Data_per_bev
 from data.acc_hospidata import H_Rate
 from data.acc_vaccinedata import V_Datensatz_erstellen, V_Dict_LK
 
-CperB_df =pd.DataFrame.from_dict(C_Data_per_bev(),orient='columns')
-
-HperB_df =pd.DataFrame.from_dict(H_Rate(),orient='columns')
 
 
 
+def Create_C_Data():
+    C_Data = pd.DataFrame.from_dict(C_Data_per_bev(), orient='columns')
+    C_Data.to_pickle('C_Data.pkl')
+    return C_Data
+
+def Load_C_Data():
+    val = input("Create [C] or load [L] Data")
+    if val in ["C", "c"]:
+        return Create_C_Data()
+    else:
+        try:
+            C_Data = pd.read_pickle('../data_analysis/C_Data.pkl')
+        except IOError:
+            print("File could not be found, creating file now...")
+            C_Data = Create_C_Data()
+        return C_Data
 
 def DF_V ():
     data = V_Dict_LK()
@@ -31,22 +44,23 @@ def DF_V ():
     return df1,df2
 
 
+def PrintDFCorr():
+    CperB_df = pd.DataFrame.from_dict(C_Data_per_bev(), orient='columns')
+
+    HperB_df = pd.DataFrame.from_dict(H_Rate(), orient='columns')
+    D1= 700
+    HperCperB_df = (HperB_df[D1:] / CperB_df[D1:])
+    V1_df, V2_df =DF_V()
+    print("C per B:\n", CperB_df[D1:])
+    print("H per B:\n", HperB_df[D1:])
+    print("V1 per B:\n", V1_df[D1:])
+    print("H/C per B:\n", HperCperB_df)
+    print("Corr V1 C per B:\n", V1_df[D1:].corrwith(CperB_df[D1:]))
+    print("Corr V1 H per B:\n", V1_df[D1:].corrwith(HperB_df[D1:]))
+    print("Corr V1 H/C per B:\n", V1_df[D1:].corrwith(HperCperB_df))
+    print("Corr V1 C per B mean:\n", V1_df[D1:].corrwith(CperB_df[D1:]).mean())
+    print("Corr V1 H per B mean:\n", V1_df[D1:].corrwith(HperB_df[D1:]).mean())
+    print("Corr V1 H/C per B mean:\n", V1_df[D1:].corrwith(HperCperB_df).mean())
 
 
 
-
-D1= 700
-print("C per B:\n",CperB_df[D1:])
-V1_df, V2_df =DF_V()
-print("H per B:\n",HperB_df[D1:])
-print("V1 per B:\n",V1_df[D1:])
-HperCperB_df= (HperB_df[D1:]/CperB_df[D1:])
-print("H/C per B:\n",HperCperB_df)
-
-print("Corr V1 C per B:\n",V1_df[D1:].corrwith(CperB_df[D1:]))
-print("Corr V1 H per B:\n",V1_df[D1:].corrwith(HperB_df[D1:]))
-print("Corr V1 H/C per B:\n",V1_df[D1:].corrwith(HperCperB_df))
-print("Corr V1 C per B mean:\n",V1_df[D1:].corrwith(CperB_df[D1:]).mean())
-print("Corr V1 H per B mean:\n",V1_df[D1:].corrwith(HperB_df[D1:]).mean())
-print("Corr V1 H/C per B mean:\n",V1_df[D1:].corrwith(HperCperB_df).mean())
-#print(CperB_df.corrwith(HperB_df))
--- visualization/visualization.py
+++ visualization/visualization.py
@@ -16,7 +16,7 @@ def plot_data(V_Data, C_Data, date_range=standard_date_range):
     y = []
 
 # Für jedes Datum jeder lk_id in CData Neuinfektionen des LK auf Impfquote des BL des LK plotten
-    for i in range (2):
+    for i in range (1):
         for lk_id in C_Data:
             for Date in C_Data[lk_id]:
                 if int(lk_id/1000) in V_Data and Date in V_Data[int(lk_id/1000)] and Date in date_range:
@@ -35,8 +35,16 @@ def TimeRange (W):
     date_list = pd.date_range(datetime.date(W[0], W[1], W[2]), datetime.date(W[3], W[4], W[5])).strftime('%Y-%m-%d').tolist()
     return date_list
 
-
-
+def plot_LK (C_Data):
+    y= []
+    x= []
+    for i in range(1000,1004):
+        for date in C_Data[i]:
+            x.append(date)
+            y.append(C_Data[i][date])
+        plt.plot(x, y, "rx", markersize=0.1)
+        plt.show()
+        x.clear(), y.clear()
 
 #plot_data(V_Dict, C_Dict)
 W1 = [2020, 3, 1, 2020, 6, 1]
@@ -44,9 +52,11 @@ W2 = [2020, 6, 1, 2021, 3, 1]
 W3 = [2021, 3, 1, 2022, 2, 16]
 W2u3 = [2020, 6, 1, 2022, 2, 16]
 #plot_data(V_Dict, C_Dict, TimeRange(W2u3))
-plot_data(V_Dict, C_Data_per_bev(), TimeRange(W2))
-plot_data(V_Dict, H_Rate(), TimeRange(W2))
-plot_data(V_Dict, C_Data_per_bev(), TimeRange(W3))
-plot_data(V_Dict, H_Rate(), TimeRange(W3))
-plot_data(V_Dict, C_Data_per_bev(), TimeRange(W2u3))
-plot_data(V_Dict, H_Rate(), TimeRange(W2u3))
+#plot_data(V_Dict, C_Data_per_bev(), TimeRange(W2))
+#plot_data(V_Dict, H_Rate(), TimeRange(W2))
+#plot_data(V_Dict, C_Data_per_bev(), TimeRange(W3))
+#plot_data(V_Dict, H_Rate(), TimeRange(W3))
+#plot_data(V_Dict, C_Data_per_bev(), TimeRange(W2u3))
+#plot_data(V_Dict, H_Rate(), TimeRange(W2u3))
+
+plot_LK(C_Dict),
